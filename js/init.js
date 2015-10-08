29e074f63ee68c5b1c39fb7ecf2f9f47253d324f
orbitus.orbitusrobotics.com <orbitus@orbitusrobotics.com>
2015-10-07 20:08:46 -0500

    fastcommit

--- js/init.js
+++ js/init.js
@@ -80,14 +80,13 @@ CKCatalog.init = function() {
     if ( window.location !== window.parent.location ) {
       // The page is in an iframe
       console.log("The page is in an iframe");
-      var searchContainer = document.getElementById("searchContainer");
-      searchContainer.style.display = "block";
-
-
 
     } else {
       // The page is not in an iframe
       console.log("The page is not in an iframe");
+      var searchContainer = document.getElementById("searchContainer");
+      searchContainer.style.display = "block";
+
     }
 
   } catch (e) {
