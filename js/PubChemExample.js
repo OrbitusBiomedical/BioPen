70b1ebf8d1b1d55b919bbb0ef696efbc508e6ee8
orbitus.orbitusrobotics.com <orbitus@orbitusrobotics.com>
2015-10-12 17:30:33 -0500

    fast-changes

--- js/PubChemExample.js
+++ js/PubChemExample.js
@@ -967,7 +967,7 @@ deviceOrientation
 	if (stereo)
 	{
 		effect = new THREE.StereoEffect( renderer );
-		effect.eyeSeparation = 20;
+		effect.eyeSeparation = 2;
 		effect.setSize( window.innerWidth, window.innerHeight );
 	}
 	else
