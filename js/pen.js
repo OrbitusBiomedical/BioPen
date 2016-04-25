407cb5f9cc88e00727c23f2b44b7342ff962081c
Orbitus <orbitus@orbitusrobotics.com>
2016-04-24 20:11:47 -0500

    untitled document creation successful

--- js/pen.js
+++ js/pen.js
@@ -7,27 +7,24 @@ CKCatalog.init_pen = function (){
 
     if (param1var == null)
     {
-      
-
       function generateUUID() {
-      var d = new Date().getTime();
-      if(window.performance && typeof window.performance.now === "function"){
-          d += performance.now();; //use high-precision timer if available
-      }
-      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-          var r = (d + Math.random()*16)%16 | 0;
-          d = Math.floor(d/16);
-          return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-      });
-    console.log("creating new pen: " + uuid);
-      return uuid;
-  };
+        var d = new Date().getTime();
+        if(window.performance && typeof window.performance.now === "function"){
+            d += performance.now();; //use high-precision timer if available
+        }
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+        });
+        console.log("creating new pen: " + uuid);
+        return uuid;
+      };
 
 
       var zoneName = undefined;
       var recordName = generateUUID();
-      var title = "title";
-      var molecule = "ether";
+      var title = "untitled";
 
       var container = CloudKit.getDefaultContainer();
   var publicDB = container.publicCloudDatabase;
@@ -41,9 +38,6 @@ CKCatalog.init_pen = function (){
     recordType: 'BioPen',
 
     fields: {
-      molecule: {
-        value: molecule
-      },
       title: {
         value: title
       }
